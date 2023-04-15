939e0995faa8f38e9ddef3fb0addbba300ddf974
jihyeok <kidjh@naver.com>
2023-04-15 19:23:43 +0900

    디자인 및 언어선택기능 추가

--- 2028_app.js
+++ 2028_app.js
@@ -2,12 +2,20 @@ let l = 5; //사용자가 원하는 단어 길이
 let k = 0;
 let correct = 0;
 let t = 0;
+let fc = 0;
+let selectedLang = 1;
 let flag = false;
 var answer;
 var hintAnswer;
 const $wrap = document.querySelector('.wrap');
 const $btn = document.querySelector('button');
 const $div = document.querySelector('div.temp');
+const $keyboxReset = document.querySelectorAll('.keyboardBox');
+const $chanceNum = document.querySelector('.status .tool:first-child em');
+
+
+
+
 
 //영어단어 데이터
 const eng = [
@@ -17,7 +25,7 @@ const eng = [
     'bottom', 'attack', 'travel', //6~8번
 ];
 
-const hints = [
+const ehints = [
     '그리고', '하지만', '소', //0~2번
     '안으로', '바위', '선물', //3~5번
     '보컬', '활용', '호버', //6~8번
@@ -31,100 +39,114 @@ const kor = [
     'ㅅㅓㄴㅂㅜㄹ', 'ㄱㅗㅏㅇㅇㅓ', 'ㅁㅏㅇㅜㅅㅡ' // 9~11번
 ];
 
-const kHint = [
-    '우루사',
+const khints = [
+    '우루사', '알코올', '발 말고',
+    '오늘', '컴포즈', '동해',
+    '바둑', '닌자', '설탕',
+    '후불', '우럭', '키보드'
 ];
 
+// 언어 선택
+const $sLang = document.querySelector('.sLang');
+$sLang.addEventListener('change', e => {
+
+    selectedLang = +$sLang.value;
+    k++;
+    if (k !== 1) {
+        $div.replaceChildren();
+        init();
+    } else {
+        init();
+    }
+
+});
 
 
 // 글자 수 선택
-
-
 const $sNum = document.querySelector('.sNum');
 $sNum.addEventListener('change', e => {
 
     l = +$sNum.value;
-    console.log(l);
     k++;
     if (k !== 1) {
         $div.replaceChildren();
-        engInit();
+        init();
     } else {
-        engInit();
+        init();
 
     }
 
 });
 
-// 언어 선택 하는 로직 짜다가 실패
-// const $sLang = document.querySelector('.sLang');
-// $sLang.addEventListener('change', e => {
-//     console.log($sLang.value);
-//     if ($sLang.value === 2) { // 게임시작 화면 한국어 선택 시
-//         k++;
-//         if (k !== 1) {
-//             $div.replaceChildren();
-//             korInit();
-//         } else {
-//             $div.replaceChildren();
-//             korInit();
-//         }
-//     } else { // 게임시작 화면 영어 선택 시
-//         k++;
-//         if (k !== 1) {
-//             $div.replaceChildren();
-//             engInit();
-//         } else {
-//             engInit();
-//         }
-//     }
-// });
+function init() {
 
-
-
-
-// 언어: 영어 선택 시 실행
-
-function engInit() {
     //랜덤단어 선정
-    
     if (l == 3) {
         var ran3 = Math.floor(Math.random() * 3);
-        answer = eng[ran3];
-        hintAnswer = hints[ran3];
-
+        if (selectedLang === 1) {
+            answer = eng[ran3];
+            hintAnswer = ehints[ran3];
+        } else {
+            answer = kor[ran3];
+            hintAnswer = khints[ran3];
+        }
+        
+        
     } else if (l == 4) {
         var ran4 = Math.floor(Math.random() * 3) + 3;
-        answer = eng[ran4];
-        hintAnswer = hints[ran4];
-
+        if (selectedLang === 1) {
+            answer = eng[ran4];
+            hintAnswer = ehints[ran4];
+        } else {
+            answer = kor[ran4];
+            hintAnswer = khints[ran4];
+        }
+        
+        
     } else if (l == 5) {
         var ran5 = Math.floor(Math.random() * 3) + 6;
-        answer = eng[ran5];
-        hintAnswer = hints[ran5];
-
+        if (selectedLang === 1) {
+            answer = eng[ran5];
+            hintAnswer = ehints[ran5];
+        } else {
+            answer = kor[ran5];
+            hintAnswer = khints[ran5];
+        }
+        
     } else if (l == 6) {
         var ran6 = Math.floor(Math.random() * 3) + 9;
-        answer = eng[ran6];
-        hintAnswer = hints[ran6];
+        if (selectedLang === 1) {
+            answer = eng[ran6];
+            hintAnswer = ehints[ran6];
+        } else {
+            answer = kor[ran6];
+            hintAnswer = khints[ran6];
+        }
 
     };
     console.log('정답지 : ' + answer);
     console.log('힌트 : ' + hintAnswer);
+    $chanceNum.textContent = answer.length + 2;
+    flag = false;
 
 
 
     //글자수대로 초기박스생성
+    const $div = document.querySelector('div.temp');
+
 
     for (let i = 0; i < answer.length; i++) {
         //input 생성
+
         const $new = document.createElement('input');
+
         $new.setAttribute('type', 'text');
         $new.setAttribute('maxlength', '1');
-        $new.setAttribute('onkeyup', "moveFocus(" + i + ")"); // 키보드이벤트1 추가
-        console.log('dddd' + answer);
+        $new.setAttribute('onkeyup', "moveFocus(" + i + ")"); // 키보드이벤트1 추가       
         $new.setAttribute('class', 'list');
+
         $div.appendChild($new);
+
 
         //마지막박스 생성 후
         if (i === answer.length - 1) {
@@ -133,99 +155,46 @@ function engInit() {
             $newbtn.classList.add('hell');
             $newbtn.textContent = '확인';
             $div.appendChild($newbtn);
+
+            $newbtn.onclick = function (e) {
+                const $div = document.querySelector('div.temp');
+                if (!e.target.matches('.temp button')) return;
+                const $input = $div.querySelectorAll('input');
+                check($div, $input);
+
+                if (!flag) creatInput($div);
+
+            };
+
         }
         $new.style.width = (500 / answer.length) + "px";
         $new.style.height = (500 / answer.length) + "px";
+
+
     }
-
 }
-
-// 언어: 한국어 선택 시 실행
-
-// function korInit() {
-//     //랜덤단어 선정
-    
-//     if (l == 3) {
-//         var ran3 = Math.floor(Math.random() * 3);
-//         answer = kor[ran3];
-
-//     } else if (l == 4) {
-//         var ran4 = Math.floor(Math.random() * 3) + 3;
-//         answer = kor[ran4];
-
-//     } else if (l == 5) {
-//         var ran5 = Math.floor(Math.random() * 3) + 6;
-//         answer = kor[ran5];
-
-//     } else if (l == 6) {
-//         var ran6 = Math.floor(Math.random() * 3) + 9;
-//         answer = kor[ran6];
-
-//     };
-//     console.log('정답지 : ' + answer);
-
-
-
-//     //글자수대로 초기박스생성
-
-//     for (let i = 0; i < answer.length; i++) {
-//         //input 생성
-//         const $new = document.createElement('input');
-//         $new.setAttribute('type', 'text');
-//         $new.setAttribute('maxlength', '1');
-//         $new.setAttribute('onkeyup', "moveFocus(" + i + ")"); // 키보드이벤트1 추가
-//         console.log('dddd' + answer);
-//         $new.setAttribute('class', 'list');
-//         $div.appendChild($new);
-
-//         //마지막박스 생성 후
-//         if (i === answer.length - 1) {
-//             //다음 버튼생성
-//             const $newbtn = document.createElement('button');
-//             $newbtn.classList.add('hell');
-//             $newbtn.textContent = '확인';
-//             $div.appendChild($newbtn);
-//         }
-//         $new.style.width = (500 / answer.length) + "px";
-//         $new.style.height = (500 / answer.length) + "px";
-//     }
-
-// }
-
-
 
 
 
 //키보드 이벤트1 : 포커스제어===============
 function moveFocus(i) {
-
-    // console.log(answer);
-
-    const $inputFocus = document.querySelectorAll('.list');
+    const $input = document.querySelectorAll('.list');
     if (i == answer.length - 1 && event.key == 'Enter') { //마지막칸 enter 체점 후 다음라인 생성
         const $div = document.querySelector('div.temp');
-        const $input = document.querySelectorAll('.list');
         check($div, $input);
+
         if (!flag) creatInput($div);
-        $inputFocus[i].blur();
+
+        $input[i].blur();
         return;
     }
 
     if (event.key == 'Escape') { //esc 누르면 input 포커스 아웃  
-        $inputFocus[i].blur();
+        $input[i].blur();
     }
-    if (!(i == answer.length - 1) && event.keyCode !== 8) $inputFocus[i + 1].focus();
+    if (!(i == answer.length - 1) && event.keyCode !== 8) $input[i + 1].focus();
 }
 
-//마우스 이벤트===============
-$wrap.addEventListener('click', e => {
-
-    const $div = document.querySelector('div.temp');
-    if (!e.target.matches('.temp button')) return; //버튼말고는 클릭이벤트 없음
-    const $input = e.target.parentNode.querySelectorAll('input');
-    check($div, $input);
-    if (!flag) creatInput($div);
-});
 
 
 //글자 채점 로직 함수===============
@@ -235,11 +204,19 @@ function check($div, $input) {
     for (let i = 0; i < answer.length; i++) {
         if ($input[i].value == answer[i]) {
             //맞는글자 키보드 초록변경
-            $input[i].style.background = 'green';
+            $input[i].style.backgroundImage = 'url(./img/green.png);';
+            $input[i].classList.add('greenbox');
+            setTimeout(function () {
+                $input[i].classList.remove('greenbox');
+
+            }, 6000);
+
             for (let j = 0; j < 26; j++) {
                 const keyboxValue = $keybox[j].getAttribute('value');
                 if (keyboxValue === $input[i].value) {
                     $keybox[j].classList.add('green');
+
+
                 }
             }
             correct++;
@@ -249,6 +226,7 @@ function check($div, $input) {
 
         } else if (answer.includes($input[i].value) && $input[i].value !== '') {
             $input[i].style.background = 'yellow';
+            $input[i].classList.add('greenbox');
 
             //맞는글자 키보드 노랑변경
             for (let j = 0; j < 26; j++) {
@@ -272,56 +250,89 @@ function check($div, $input) {
             $input[i].style.background = 'lightgrey';
         }
     }
-    const $inputFocus = document.querySelectorAll('.list');
-    for (let i = 0; i < answer.length; i++) {
-        $inputFocus[i].classList.remove('list'); //기존 키보드 이벤트1 권한 제거
-    }
+
 
     t++; //도전횟수 카운트
     if (t == answer.length + 2 && flag == false) {
         flag = true; // 실패해도 맞출때까지 박스 생성하게 해줌
         defeat(answer);
     }
-
-    // 상태창 남은 기회 숫자 올리기
-    const $chanceNum = document.querySelector('.status .tool:first-child em');
     $chanceNum.textContent = answer.length + 2 - t;
-
-
 }
-
-
-
-
-
-
-
 
 
 //다음라인 생성 함수===============
 function creatInput($div) {
+
+    const $inputFocus = document.querySelectorAll('.list');
+
+    for (let i = 0; i < answer.length; i++) {
+
+        $inputFocus[i].classList.remove('list'); //기존 키보드 이벤트1 권한 제거
+    }
+
+
     const $clone = $div.cloneNode(true);
+
+    $clone.lastChild.onclick = function (e) {
+        const $div = document.querySelector('div.temp');
+        if (!e.target.matches('.temp button')) return;
+        const $input = $div.querySelectorAll('input');
+        check($div, $input);
+
+        if (!flag) creatInput($div);
+
+    };
+
     const $inputs = $clone.querySelectorAll('input');
+
     for (let $ele of [...$inputs]) {
+        $ele.classList.remove('greenbox');
+
         $ele.value = '';
         $ele.style.background = 'white';
         $ele.classList.add('list'); //새로운 라인 키보드 이벤트1 권한 부여
     }
-
     const $btn = document.querySelector('.hell');
     $btn.remove(); //지난라인 버튼제거            
     $wrap.appendChild($clone);
     $div.classList.remove('temp');
+    $div.classList.add('remove');
+
     $inputs[0].focus(); // 생성된 다음라인 첫칸 포커스
     correct = 0; //녹색갯수 초기화
     $wrap.scrollTop = $wrap.scrollHeight; //스크롤 최하단 유지
 }
 
+function reset() {
+
+    const $remove = document.querySelectorAll('.remove');
+    for (let i = 0; i < $remove.length; i++) {
+        $remove[i].remove();
+    }
+
+    const $rtemp = document.querySelector('.temp');
+    $rtemp.replaceChildren();
+    for (let i = 0; i < 26; i++) {
+        $keyboxReset[i].classList.remove('green');
+        $keyboxReset[i].classList.remove('yellow');
+        $keyboxReset[i].classList.remove('lightgrey');
+    }
+    t = 0;
+
+    init();
+    const $remov = document.querySelector('.remove');
+    $remov.remove();
+
+}
+
+
 
 //승리시 정답함수===============
 function victory(answer) {
+
     alert('성공입니다!');
-    flag = true // 승리시 추가 라인 생성없음
+    flag = true; // 승리시 추가 라인 생성없음
 
     //성공단어 사이드로
     const $vpad = document.getElementById('vpad');
@@ -329,6 +340,8 @@ function victory(answer) {
     $sword.classList.add('sword');
     $sword.textContent = answer;
     $vpad.appendChild($sword);
+
+    reset();
 };
 
 function defeat(answer) {
@@ -340,6 +353,7 @@ function defeat(answer) {
     $dword.classList.add('dword');
     $dword.textContent = answer;
     $dpad.appendChild($dword);
+    reset();
 }
 
 // 상태 창
@@ -386,7 +400,7 @@ $selectLang.addEventListener('click', e => {
 
 const $startPage = document.querySelector('.startPage');
 const $startBtn = document.querySelector('#startBtn');
-const $sbtn2 = document.querySelector('.sbtn');
+const $sbtn2 = document.querySelector('#sbtn2');
 const $intro = document.querySelector('.intro');
 const $introText = document.querySelector('.introText');
 const $select = document.querySelector('.select');
@@ -394,8 +408,48 @@ const $final = document.querySelector('.final');
 
 $startPage.addEventListener('click', e => {
     if (e.target === $startBtn) {
-        $startPage.style.display = 'none';
-        $select.style.display = 'block';
+
+        const $ka = document.querySelector('#ka');
+        const $kb = document.querySelector('#kb');
+        const $k1 = document.querySelector('#k1');
+        const $k2 = document.querySelector('#k2');
+
+        $ka.style.display = 'block';
+        $kb.style.display = 'block';
+        setTimeout(function () {
+            $startPage.style.display = 'none';
+            $select.style.display = 'block';
+            $ka.style.display = 'none';
+            $kb.style.display = 'none';
+            $k1.style.display = 'block';
+            $k2.style.display = 'block';
+
+        }, 2000);
+
+    }
+});
+
+$sbtn2.addEventListener('click', e => {
+    if (e.target === $sbtn2) {
+        
+        const $kc = document.querySelector('#kc');
+        const $kd = document.querySelector('#kd');
+        const $k3 = document.querySelector('#k3');
+        const $k4 = document.querySelector('#k4');
+
+
+        $kc.style.display = 'block';
+        $kd.style.display = 'block';
+        setTimeout(function () {
+            $select.style.display = 'none';
+            $final.style.display = 'flex';
+            $kc.style.display = 'none';
+            $kd.style.display = 'none';
+            $k3.style.display = 'block';
+            $k4.style.display = 'block';
+
+        }, 3000);
+
     }
 });
 
@@ -408,22 +462,28 @@ function intros2() {
     $introText.style.display = 'none';
 }
 
-function startClick() {
-    $select.style.display = 'none';
-    $final.style.display = 'flex';
 
-}
+
+// // function startClick() {
+// //     const $kc = document.querySelector('#kc');
+// //     const $kd = document.querySelector('#kd');
+// //     const $k3 = document.querySelector('#k3');
+// //     const $k4 = document.querySelector('#k4');
+// $kc.style.display = 'block';
+// $kd.style.display = 'block';
+// $select.style.display = 'none';
+// $final.style.display = 'flex';
+
+// // }
 
 // 0414 힌트 생성 로직
 const $btnHint = document.querySelector('.btn-hint');
 const $hintText = document.querySelector('.hint-text');
 
+
 $btnHint.addEventListener('click', e => {
     $hintText.classList.toggle('hide');
-    if ($hintText.classList.contains('hide')) {
-        $hintText.textContent = '';
-    } else {
-        $hintText.textContent = hintAnswer;
-    }
-
+    $hintText.textContent = hintAnswer;
 });
+
+// 인게임 글자 수 선택 기능
